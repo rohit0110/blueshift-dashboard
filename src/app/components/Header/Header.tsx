c8406953da9d6a1d5481fa02a4368411398f5e18
Jyezy <jyezy@Jyezys-MacBook-Pro.local>
2026-01-29 17:32:17 +0700

    bump design system with navigation

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -86,6 +86,7 @@ export default function HeaderContent() {
               </Link>
 
               <Tabs
+                isNavigation
                 items={[
                   {
                     label: t("header.paths"),
